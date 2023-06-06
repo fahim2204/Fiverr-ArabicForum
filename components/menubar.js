095c9f9e0cb4e6e405d5d7945099a3f8b903b688
Tasiful alam <tasifulalam.9846@gmail.com>
2023-06-07 00:08:31 +0600

    change navbar hover color

--- components/menubar.js
+++ components/menubar.js
@@ -46,13 +46,13 @@ const Menubar = () => {
 
   return (
     <>
-      <nav className="border-b w-full bg-green-500">
+      <nav className="border-b w-full bg-green-600">
         <ul className="sm:max-w-6xl md:mx-auto text-white flex flex-row-reverse justify-around items-center">
           {menuList.map((item, index) => {
             return (
               <li
                 key={index}
-                className="hover:bg-orange-300 py-3 px-5 transition-all duration-300 cursor-pointer border-b-transparent hover:border-b-orange-700 border-b-2"
+                className="hover:bg-green-400 py-3 px-5 transition-all duration-300 cursor-pointer border-b-transparent hover:border-b-gray-700 border-b-2"
               >
                 <Link href={item.link}>
                   <span>{item.name}</span>
